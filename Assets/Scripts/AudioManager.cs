334a891ef2f97e489120f0a084a9b7f36d8e0f4b
Anngel Ortiz <anngelo.omz@gmail.com>
2023-11-13 16:04:24 -0600

    Funcionalidad volumen

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,14 +1,46 @@
 using UnityEngine.Audio;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    AudioMixer audioMixer;
+    public AudioMixer audioMixer;
+    public Slider volMaster;
+    public Slider volFX;
+    public AudioSource fxaudioSource;
+    public AudioClip clickAudio;
+    public Toggle mute;
+    private float lastVolume;
 
-    public void ChangeVolumeMaster(float volume) {
+    void Awake()
+    {
+        volFX.onValueChanged.AddListener(ChangeVolumeFX);
+        volMaster.onValueChanged.AddListener(ChangeVolumeMaster);
+    }
+
+    public void SetMute()
+    {
+        
+        if (mute.isOn){
+            audioMixer.GetFloat("VolMaster", out lastVolume);
+            audioMixer.SetFloat("VolMaster", -80);
+        }
+        else
+            audioMixer.SetFloat("VolMaster", lastVolume);
+        PlayClick();
+    }
+
+    public void ChangeVolumeMaster(float volume)
+    {
         audioMixer.SetFloat("VolMaster", volume);
     }
-    public void ChangeVolumeFX(float volume) {
+    public void ChangeVolumeFX(float volume)
+    {
         audioMixer.SetFloat("VolFX", volume);
+    }
+
+    public void PlayClick()
+    {
+        fxaudioSource.PlayOneShot(clickAudio);
     }
 }

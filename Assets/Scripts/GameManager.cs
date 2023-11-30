5e3ba918e355a4822e08cb5cfc032548270d6ed5
Anngel Ortiz <anngelo.omz@gmail.com>
2023-11-30 14:39:36 -0600

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] private float maxTime;
+    private float currentTime;
+    private bool activateTime = false;
+
+    void Start()
+    {
+        ActivateTemporizer();
+    }
+
+    void Update()
+    {
+        if (activateTime)
+        {
+            ChangeCounter();
+        }
+    }
+
+    private void ChangeCounter() {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0) {
+            Debug.Log("Derrota");
+            DesactivateTemporizer();
+        }
+    }
+
+    private void ChangeTemporizer(bool state) {
+        activateTime = state;
+    }
+
+    public void ActivateTemporizer() {
+        currentTime = maxTime;
+        ChangeTemporizer(true);
+    }
+
+    public void DesactivateTemporizer() {
+        ChangeTemporizer(false);
+    }
 }

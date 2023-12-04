1ad38edbe6eb59779e843a5b96df50bccb3152ad
FranBulk <leo.bulkley@gmail.com>
2023-12-04 02:07:23 -0600

    Cambios

---
+++ Assets/Scripts/Conv/Dialogue.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}

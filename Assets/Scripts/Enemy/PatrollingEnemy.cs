2f34479f6d04818f8a11a81026bf08799653b4ca
Ivan Urrutia <iurrutia2468@gmail.com>
2023-12-04 01:19:17 -0600

    Detalles de enemigos parte 1

---
+++ Assets/Scripts/Enemy/PatrollingEnemy.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PatrollingEnemy : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float moveSpeed = 3f;
+
+    private int currentPatrolIndex = 0;
+    private bool isMovingForward = true;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Patrol();
+    }
+
+    void Patrol()
+    {
+
+
+ 
+        Vector2 target = patrolPoints[currentPatrolIndex].position;
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        if (transform.position.x < target.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+
+
+        if (Vector2.Distance(transform.position, target) < 0.1f)
+        {
+         
+            if (isMovingForward)
+            {
+                currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+            }
+            else
+            {
+                currentPatrolIndex = (currentPatrolIndex - 1 + patrolPoints.Length) % patrolPoints.Length;
+            }
+        }
+    }
+}
